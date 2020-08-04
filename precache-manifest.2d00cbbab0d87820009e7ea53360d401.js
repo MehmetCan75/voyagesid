a5d0d75f7ad2f83c60f57d2ac6acedc82547c5bf
MehmetCan75 <mehmetozmenfb@gmail.com>
2020-08-04 12:52:35 +0200

    Updates

---
+++ precache-manifest.2d00cbbab0d87820009e7ea53360d401.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a479e741bff5a52c2004d26918bf4dd",
+    "url": "/voyagesid/index.html"
+  },
+  {
+    "revision": "36c48711a8135bd836e3",
+    "url": "/voyagesid/static/css/main.1d9b8459.chunk.css"
+  },
+  {
+    "revision": "149f9f00f0bd633c996f",
+    "url": "/voyagesid/static/js/2.6fb69059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/voyagesid/static/js/2.6fb69059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36c48711a8135bd836e3",
+    "url": "/voyagesid/static/js/main.60dbe78c.chunk.js"
+  },
+  {
+    "revision": "78a09afa3ebe79a73b77",
+    "url": "/voyagesid/static/js/runtime-main.e395e0de.js"
+  },
+  {
+    "revision": "8d19350bc8f3e657a170e6419e5c823e",
+    "url": "/voyagesid/static/media/beach.8d19350b.jpg"
+  },
+  {
+    "revision": "26631a4043b14dff84180bdf51c3cacb",
+    "url": "/voyagesid/static/media/instagram (1).26631a40.png"
+  },
+  {
+    "revision": "909f55051e5391eb55e386bc0c826afa",
+    "url": "/voyagesid/static/media/lisboa.909f5505.jpg"
+  },
+  {
+    "revision": "e7e9ae0f91244b76283c9fc030df898f",
+    "url": "/voyagesid/static/media/naples.e7e9ae0f.jpg"
+  },
+  {
+    "revision": "465ea85a5aa566cce2a81d31f7506ede",
+    "url": "/voyagesid/static/media/photo-1487349384428-12b47aca925e.465ea85a.jpeg"
+  },
+  {
+    "revision": "0f53112799a9b594e2ab4ddd4fd7555c",
+    "url": "/voyagesid/static/media/photo-1549849171-09f62448709e.0f531127.jpeg"
+  },
+  {
+    "revision": "a70d773c42bec2e14c420ea03f6cf5e2",
+    "url": "/voyagesid/static/media/photo-1558782630-3dff4371ad7e.a70d773c.jpeg"
+  },
+  {
+    "revision": "2ff50ffc555b62c687026b6be4a0fc12",
+    "url": "/voyagesid/static/media/split.2ff50ffc.jpg"
+  }
+]);
