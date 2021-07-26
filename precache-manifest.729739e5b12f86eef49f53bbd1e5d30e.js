0fc6e8fbfc496e5244ea7c57acb49bca77355b8a
Mehmet <mehmetozmenfb@gmail.com>
2021-07-26 12:52:30 +0200

    Updates

--- precache-manifest.729739e5b12f86eef49f53bbd1e5d30e.js
+++ precache-manifest.729739e5b12f86eef49f53bbd1e5d30e.js
@@ -1,43 +1,35 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7b44241a2c77049e5bfb8735a1afd111",
+    "revision": "8dd861e8373f411ef1a93bf0c8435f00",
     "url": "/voyagesid/index.html"
   },
   {
-    "revision": "69293f4b9aa235ad0dd6",
+    "revision": "7fad12193159e11262e3",
     "url": "/voyagesid/static/css/main.7f3f69e0.chunk.css"
   },
   {
-    "revision": "294a0b44639199268d8b",
-    "url": "/voyagesid/static/js/2.cddb141c.chunk.js"
+    "revision": "7b44a501e64c159ea639",
+    "url": "/voyagesid/static/js/2.2935da63.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/voyagesid/static/js/2.cddb141c.chunk.js.LICENSE.txt"
+    "url": "/voyagesid/static/js/2.2935da63.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "69293f4b9aa235ad0dd6",
-    "url": "/voyagesid/static/js/main.e92dbeb4.chunk.js"
+    "revision": "7fad12193159e11262e3",
+    "url": "/voyagesid/static/js/main.127c7640.chunk.js"
   },
   {
     "revision": "78a09afa3ebe79a73b77",
     "url": "/voyagesid/static/js/runtime-main.e395e0de.js"
   },
   {
-    "revision": "a2afaa3724a897bccd1cf1f89cbf80bd",
-    "url": "/voyagesid/static/media/Brasilia.a2afaa37.jpeg"
-  },
-  {
-    "revision": "3590259bf04fb0e61031a58a897b0819",
-    "url": "/voyagesid/static/media/CampoGrande.3590259b.jpeg"
-  },
-  {
     "revision": "71d8bb9561cbbfee8249e7cec5f995f8",
     "url": "/voyagesid/static/media/Gol.71d8bb95.png"
   },
   {
-    "revision": "b939f765888f03de4d53301bfa4c25f7",
-    "url": "/voyagesid/static/media/Latam.b939f765.jpg"
+    "revision": "91d3e79c92aab6c251bc6131a087c377",
+    "url": "/voyagesid/static/media/PortoAlegre.91d3e79c.jpeg"
   },
   {
     "revision": "bd7e2a398aa88baa165fdc744327a758",
@@ -46,6 +38,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "8d19350bc8f3e657a170e6419e5c823e",
     "url": "/voyagesid/static/media/beach.8d19350b.jpg"
+  },
+  {
+    "revision": "ac78a0953e6580ecddcfdfff6819e9e3",
+    "url": "/voyagesid/static/media/goiania.ac78a095.jpeg"
   },
   {
     "revision": "26631a4043b14dff84180bdf51c3cacb",
