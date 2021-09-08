d5149e12c631ff5a2bda06eea8eaea018c2d8180
Mehmet <mehmetozmenfb@gmail.com>
2021-09-08 22:01:37 +0200

    Updates

--- precache-manifest.52966a879433604d44902674b0bb62f5.js
+++ precache-manifest.52966a879433604d44902674b0bb62f5.js
@@ -1,35 +1,35 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a986537f545f96b1a9182b678d55a22e",
+    "revision": "67ff0b124a4db2dbcf755c0333a98453",
     "url": "/voyagesid/index.html"
   },
   {
-    "revision": "8aac51f3765ecc89959f",
+    "revision": "3179ae0577be3d4c656f",
     "url": "/voyagesid/static/css/main.79896ad8.chunk.css"
   },
   {
-    "revision": "18190bc3bf1c5a6598ef",
-    "url": "/voyagesid/static/js/2.cb995979.chunk.js"
+    "revision": "ad655ac9a2d71bf8489c",
+    "url": "/voyagesid/static/js/2.56dd27d3.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/voyagesid/static/js/2.cb995979.chunk.js.LICENSE.txt"
+    "url": "/voyagesid/static/js/2.56dd27d3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8aac51f3765ecc89959f",
-    "url": "/voyagesid/static/js/main.92d0d25d.chunk.js"
+    "revision": "3179ae0577be3d4c656f",
+    "url": "/voyagesid/static/js/main.9a781a65.chunk.js"
   },
   {
     "revision": "78a09afa3ebe79a73b77",
     "url": "/voyagesid/static/js/runtime-main.e395e0de.js"
   },
   {
-    "revision": "a2afaa3724a897bccd1cf1f89cbf80bd",
-    "url": "/voyagesid/static/media/Brasilia.a2afaa37.jpeg"
+    "revision": "c8b0b53ce654473967f16843c729a1b2",
+    "url": "/voyagesid/static/media/Barcelona.c8b0b53c.jpeg"
   },
   {
-    "revision": "71d8bb9561cbbfee8249e7cec5f995f8",
-    "url": "/voyagesid/static/media/Gol.71d8bb95.png"
+    "revision": "b939f765888f03de4d53301bfa4c25f7",
+    "url": "/voyagesid/static/media/Latam.b939f765.jpg"
   },
   {
     "revision": "bd7e2a398aa88baa165fdc744327a758",
@@ -40,8 +40,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/voyagesid/static/media/beach.8d19350b.jpg"
   },
   {
-    "revision": "51843cb8e72fa1d5b5fa8402bddd422c",
-    "url": "/voyagesid/static/media/florianopolis3.51843cb8.jpeg"
+    "revision": "112c1c24621adea1b3ef307ec1a746ea",
+    "url": "/voyagesid/static/media/dublin.112c1c24.jpeg"
   },
   {
     "revision": "26631a4043b14dff84180bdf51c3cacb",
@@ -54,5 +54,9 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "0f53112799a9b594e2ab4ddd4fd7555c",
     "url": "/voyagesid/static/media/photo-1549849171-09f62448709e.0f531127.jpeg"
+  },
+  {
+    "revision": "4ec5390974ece1d15852c1face1b9281",
+    "url": "/voyagesid/static/media/tap.4ec53909.png"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/voyagesid/precache-manifest.e057b3f18a676c5fd31fdbdc010da99f.js"
+  "/voyagesid/precache-manifest.52966a879433604d44902674b0bb62f5.js"
 );
 
 self.addEventListener('message', (event) => {
